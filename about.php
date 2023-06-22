43dc2172b15db7f46b6467261cbc6d15c84e3b0a
iamvincentnam <vincentnam22@gmail.com>
2023-06-22 13:10:12 +0100

    an about page created and developed with included header & footer files

---
+++ about.php
@@ -0,0 +1,54 @@
+
+<?php include_once 'inc/header.php'; ?>
+
+
+    <section id="about" class="about-section-padding bg-white m-0 py-4">
+     
+            <div class="row">
+            <div class="col-lg-3 col-sm-12 col-12 d-flex align-items-center">
+              <div class="about-img book_title_img text-center ">
+                <img src="imgs/concubine.jpg" alt="" class="img-fluid ">
+              </div>
+            </div>
+            <div class="col-lg-9 col-md-12 col-12 ps-lg-5 mt-md-5">
+              <div class="about-text">
+                <p class="display-5 my-0">
+                 The Concubine
+                </p>
+                <p class="fs-3 text-black-50 my-2 my-lg-0"> Elechi Amadi</p>
+                <p>A novel by <span>Elechi Amadi</span></p>
+
+                <div class="d-flex justify-content-between col-lg-12 flex-column flex-sm-row">
+                    <p class="text-muted fw-bold mb-2 mb-lg-0">Genre</p>
+                    <div class="mb-2 mb-sm-0 text-left">
+                        <span class="badge bg-secondary pill py-2">History</span>
+                        <span class="badge bg-secondary pill py-2">Fiction</span>
+                        <span class="badge bg-secondary pill py-2">African Literature</span>
+
+                  </div>
+                </div>
+                <hr>
+                <div class="about_author bg-gradient-primary">
+                    <h4 class="text-secondary py-2">About The Book</h4>
+                    <div class="img_thumbnail col-12 d-inline-block pe-2 my-2">
+                    <img src="imgs/concubine.jpg" alt="" class="img-fluid m-0 p-0">
+                    </div>
+                    <h6 class="d-inline-block">Elechi Amadi</h6>
+                    
+                </div>
+                <p class="text-black- mt-4 mb-sm-4">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus natus deserunt culpa dignissimos aliquid soluta maxime aut ad tempora ratione? Eveniet, debitis deserunt aliquid rerum harum quasi optio provident similique.</p>
+
+                <a href="index.php" class="btn px-2 text-light bg-secondary mb-4 shadow"> Go Back To Home</a>
+              </div>
+            </div>
+        </div>
+ 
+      </section>
+
+      <?php include_once 'inc/footer.php'; ?>
+
+
+
+
+
+   
