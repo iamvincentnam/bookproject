394740f46bb474d3972e795a6b247c1f03d180f4
iamvincentnam <vincentnam22@gmail.com>
2023-07-10 10:28:18 +0100

    added a carousel slide and made the nav more responsive

--- inc/header.php
+++ inc/header.php
@@ -9,144 +9,243 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <style>
-          
-.img_thumbnail{width: 90px; height: 90px;}
- .img_thumbnail img{border-radius: 50%;
-    border:none;
-    width: 90px; height: 90px;}
-    .largeThumbnail{
-border: 2px solid purple;
-height: 80%;
-width: 70%;
-margin: 2rem auto;
-padding: 0 !important;
-/* border-radius: 10px; */
-position: relative;
-box-sizing: border-box;
-top:0em;
-z-index: 10;
-display: none;
-}
-    .largeThumbnail img{
-    width: 100%;
-    height: 100%;
-    padding: 0;
-    margin: 0;
-}
-.largeThumbnail #closeIcon{position: absolute;
-right:-.4rem;
-top:-10px;
-cursor: pointer;
-margin: 0;
-padding: 0;
-transition: 0.2s ease-in-out all ;
-background-color: #1b3c6a74;
-transform: scale(0.9)}
-.largeThumbnail #closeIcon:hover{
-background-color: #1b3c6a;
-transform: scale(1.1);
-}
-        body {
+     /* Body */
+body {
   --bs-body-font-family: var(--bs-font-monospace);
   --bs-body-line-height: 1.4;
   --bs-body-bg: var(--bs-gray-100);
   position: relative;
-        }
-section .logo:hover{transform: translate(.5rem);}
-section .logo{transition:.4s; cursor: pointer;}
-        input::placeholder{color: #5f5f5f95 !important;}
-.nav{ height: 60px;}
-            .nav ul {
-        list-style: none;
-    }
-    .nav ul li{display: inline-block;
-        }
-        .nav ul a{
-            text-decoration: none;
-             color: #d9d8da; 
-                 font-weight:600;}
-        .nav ul a:hover{border-bottom: 2px groove orangered;
-        color: #8e8e8e;}
-        /* section{background-color: #efb680;} */
-        /* section{min-height: 100vh;} */
-    /* the 100vh for section is not practicable since i'd including the same header file in other php pages*/
-        form label{font-size: 85%; font-weight: bold; color: rgb(61, 61, 61);} 
-        
-.material-symbols-outlined {
-  font-variation-settings:
-  'FILL' 0,
-  'wght' 400,
-  'GRAD' 0,
-  'opsz' 48
 }
 
-    .delete-icon span{cursor: pointer; }
-    .delete-icon span a{text-decoration: none; color: lightslategray;}
-    .delete-icon span:hover{scale: 1.1;}
-.container-fluid .row{margin-bottom: 5rem;}
+/* Navigation */
+nav .logo:hover {
+  transform: translate(.5rem) !important;
+}
 
-    table{min-height:auto; }
-    /* table tr:nth-of-type(n+2):hover{scale: 1.01;} */
-    /* table tr:nth-of-type(1):hover{scale: 1 } */
-    table tr{height: 50px;transition: all .2s;}
-      table td, table th{
-        cursor: pointer;
-    }
-      table tr:nth-of-type(even){
-        background-color: #d2c361e2;
-    }
-      
-      .custom-card-height{
-         height: 420px;
-        max-width: 300px;
-    }
-        .custom-card-height h5{
-            cursor: pointer;
-        }
-        .custom-card-height h5:hover,.each_book_title:hover{text-decoration: underline; color: purple !important; }
-        .each_book_title{cursor:grab;}
-      .custom-card-height .card-img-top{ max-height: 250px; /* Adjust the max-height of the image to prevent overflow */
-  object-fit:'';}
-  .author_card
-  {height: 370px;
-    max-width: 250px;}
+nav .logo {
+  transition: .4s;
+  cursor: pointer;
+}
 
-.nav_logo{text-decoration: none;}
-  .author_img
-  {
-    width: 180px; 
-    height: 180px;}
-  .go_back_to_home{
-    cursor: pointer; 
-    transition: .3s ease-in-out all;}
-  .go_back_to_home:hover
-  { background-color:#7f7f7f !important; font-weight: bold !important;}
-  .each_book_data_author_name{transition: .2s ease-in-out all;cursor: pointer;}
-  .each_book_data_author_name:hover{ text-decoration: underline; font-weight: bold;}
+/* Input */
+input::placeholder {
+  color: #5f5f5f95 !important;
+}
+
+/* Carousel */
+.carousel-inner::before {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.7);
+  z-index: 2;
+}
+
+.carousel-item img {
+  max-width: 100% !important;
+  height: auto !important;
+  margin: 0 !important;
+  padding: 0 !important;
+}
+
+/* Navigation Links */
+nav ul a {
+  text-decoration: none;
+  color: #d9d8da !important;
+  font-weight: 600;
+}
+
+nav ul a:hover {
+  border-bottom: 1px solid orangered;
+  color: #8e8e8e;
+}
+
+/* Form Label */
+form label {
+  font-size: 85%;
+  font-weight: bold;
+  color: rgb(61, 61, 61);
+}
+
+/* Book Catalogue */
+.book_catalogue {
+  margin-top: 5rem !important;
+  max-height: auto;
+}
+
+/* Miscellaneous */
+.material-symbols-outlined {
+  font-variation-settings: 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 48;
+}
+
+.delete-icon span {
+  cursor: pointer;
+}
+
+.delete-icon span a {
+  text-decoration: none;
+  color: lightslategray;
+}
+
+.delete-icon span:hover {
+  scale: 1.1;
+}
+
+.container-fluid .row {
+  margin-bottom: 5rem;
+}
+
+/* Table */
+table {
+  min-height: auto;
+}
+
+table tr {
+  height: 50px;
+  transition: all .2s;
+}
+
+table td,
+table th {
+  cursor: pointer;
+}
+
+table tr:nth-of-type(even) {
+  background-color: #d2c361e2;
+}
+
+/* Custom Card */
+.custom-card-height {
+  height: 420px;
+  max-width: 300px;
+}
+
+.custom-card-height h5 {
+  cursor: pointer;
+}
+
+.custom-card-height h5:hover,
+.each_book_title:hover {
+  text-decoration: underline;
+  color: purple !important;
+}
+
+.each_book_title {
+  cursor: grab;
+}
+
+.custom-card-height .card-img-top {
+  max-height: 250px;
+  /* Adjust the max-height of the image to prevent overflow */
+  object-fit: '';
+}
+
+/* Author Card */
+.author_card {
+  height: 370px;
+  max-width: 250px;
+}
+
+/* Navigation Logo */
+.nav_logo {
+  text-decoration: none;
+  letter-spacing: 2px;
+}
+
+/* Author Image */
+.author_img {
+  width: 180px;
+  height: 180px;
+}
+
+/* Go Back Button */
+.go_back_to_home {
+  cursor: pointer;
+  transition: .3s ease-in-out all;
+}
+
+.go_back_to_home:hover {
+  background-color: #7f7f7f !important;
+  font-weight: bold !important;
+}
+
+/* Each Book Data */
+.each_book_data_author_name {
+  transition: .2s ease-in-out all;
+  cursor: pointer;
+}
+
+.each_book_data_author_name:hover {
+  text-decoration: underline;
+  font-weight: bold;
+}
+
     </style>
+  <link rel="icon" href="imgs/icons8-book-96.png" type="image/icon type">
     <title>MyBook CATALOGUE
     </title>
 </head>
 <body>
-    <section class="section col-lg-12  col-sm-12  mx-auto  m-0 p-0 shadow bg-light">
+
+
+
+<nav class="navbar fixed-top  navbar-expand-md navbar-dark bg-dark">
+  <div class="container-fluid">
+  <a href="http://localhost/bookProject/index.php" class="lead text-white fw-bold m-0 p-0 nav_logo logo">Book<i class="bi bi-book text-danger"></i></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse " id="navbarNav">
+        <!-- <div class="mx-auto"></div> -->
+      <ul class="navbar-nav ms-auto d-flex justify-content-center align-items-center">
+      <li class="nav-item m-0 p-0 me-5  ">
+          <a href="http://localhost/bookProject/index.php" class="nav-link  <?= $_SERVER["REQUEST_URI"] == "/bookProject/index.php" ? "bg-light text-secondary py-1 px-2 rounded" : ''; ?>"> Home </a>
+        </li>
+
+        <li class="nav-item  m-0 p-0  me-5 ">
+          <a href="http://localhost/bookProject/about.php" class="nav-link <?= $_SERVER["REQUEST_URI"] == "/bookProject/about.php" ? "bg-light text-secondary py-1 px-2 rounded" : ''; ?>">About</a>
+        </li>
+
+        <li class="nav-item m-0 p-0  me-5">
+          <a href="http://localhost/bookProject/author.php" class="nav-link <?= $_SERVER["REQUEST_URI"] == "/bookProject/author.php" ? "bg-light text-secondary py-1 px-2 rounded" : ''; ?>">Authors</a>
+        </li>
+
+
+        <li class="nav-item m-0 p-0  me-0">
+          <a href="#" class="nav-link d-none d-md-inline <?= $_SERVER["REQUEST_URI"] == "/bookProject/add_to_book.php" ? "bg-light text-secondary py-1 px-2 rounded" : ''; ?>"><i class="bi bi-person-fill-add"></i> <i class="bi bi-book-half"></i></a>
+        </li>
+       
+        <li class="nav-item m-0 p-0  me-5">
+          <a href="#" class="nav-link  d-md-none <?= $_SERVER["REQUEST_URI"] == "/bookProject/add_to_book.php" ? "bg-light text-secondary py-1 px-2 rounded" : ''; ?>">ADD</a>
+        </li>
+       
+      
+      </ul>
+    </div>
+  </div>
+</nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+<script>
+    const nav = document.querySelector('nav');
+    window.addEventListener('scroll',()=>{
+        if(window.pageYOffset >100){
+            // alert("window.pageYOffset >100")
+            // nav.classList.remove('bg-dark' )
+            nav.classList.add('bg-opacity-75')
+        }else{
+            // nav.classList.add('bg-dark')
+            nav.classList.remove('bg-opacity-75')
+        }
+    }); 
+</script>
+
+    <section class="section col-lg-12  col-sm-12  mx-auto  m-0 p-0 shadow bg-light mt-5">
 
         <div class="container-fluid">
-        <div class="row">
-        
-       <div class="col-md-12 nav d-flex justify-content-between align-items-center px-3 bg-dark shadow-lg fixed-top mb-5">
-        <div class="logo col-6">
-            <a href="http://localhost/bookProject/index.php" class="lead text-white fw-bold m-0 p-0 nav_logo">Book<i class="bi bi-book text-danger"></i></i></a>
-        </div>
-        <div id="links" class="col-lg-6 flex-wrap">
-            <ul class="d-flex  justify-content-around align-items-center m-0 p-0 ">
-                <li><a href="http://localhost/bookProject/index.php" class="<?= $_SERVER["REQUEST_URI"] == "/bookProject/index.php" ? "bg-light text-secondary py-1 px-3 rounded" : ''; ?>"> Home </a></li>
-                <li><a href="http://localhost/bookProject/about.php" class="<?= $_SERVER["REQUEST_URI"] == "/bookProject/about.php" ? "bg-light text-secondary py-1 px-3 rounded" : ''; ?>">About</a></li>
-               
-                <li><a href="http://localhost/bookProject/author.php" class="<?= $_SERVER["REQUEST_URI"] == "/bookProject/author.php" ? "bg-light text-secondary py-1 px-3 rounded" : ''; ?>">Authors</a></li>
-            </ul>
-
-        </div>
-       </div>
-       </div>
+       <!-- the former nav sits here-->
+      
 
     
--- index.php
+++ index.php
@@ -1,52 +1,65 @@
-<?php  include 'inc/header.php'; ?>
+<?php include 'inc/header.php'; ?>
 <?php 
 
 $query = "SELECT *
           FROM authors
           INNER JOIN books ON authors.id = books.author_table_id
-          ORDER BY books.book_id;";
+          ORDER BY books.book_id";
 $books = fetch_Data_from_Database($query, 'fetchAll');
 
+$carousel_img_query = "SELECT * 
+                       FROM books 
+                       WHERE book_img IN ('ttayn.jpg', 'ph.jpg', 'scc.jpg', 'tpw.jpg', 'tc.jpg', 'aamd.jpg') 
+                       ORDER BY book_id";
+$carousel_img_data = fetch_Data_from_Database($carousel_img_query, 'fetchAll');
+
 ?>
-<div class="row col-12 bg-primary p-0 m-0">
-  <h1 class=" text-center text-light-50 mt-5 "> BOOK CATALOGUE</h1>
+
+<div class="row col-lg-8 p-0 m-0 book_catalogue mx-auto">
+  <div id="carouselExampleAutoplaying" class="carousel slide col-lg-4 mx-auto" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <?php foreach($carousel_img_data as $index => $each_image): ?>
+        <div class="carousel-item <?php echo ($index === 0) ? 'active' : ''; ?>">
+          <img src="imgs/<?php echo $each_image['book_img']; ?>" class="d-block m-0 p-0 img-fluid" alt="first_book_image">
+        </div>
+      <?php endforeach; ?>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
 </div>
 
-  <div class="row col-12 mx-auto my-5 ">
-
- <?php if(empty($books)): ?>
-  <p class="lead text-primary p-2 text-center fw-bold">
-  THERE IS NO Book AVAILABLE
- </p>
- <?php endif; ?>
-        <table class="table table-striped table-bordered table-hover table-responsive m-0 p-0">
-            <thead class="">
-                <tr class="bg-secondary text-white  shadow">
-                  <th scope="col" width="5%">S/N</th>
-                  <th scope="col"  width="55%" class="text-center">Title</th>
-                  <th scope="col"  width="30%" class="text-center">Author</th>
-                  <th scope="col"  width="10%">Released Year</th>
-                </tr>
-              </thead>
-              <tbody>
-          <?php foreach($books as $book):
-                ?>
-            <tr class="book_row" name="book_row" onclick="window.location.href='each_book_info.php?book_id=<?=$book['book_id'] ?>'">
-              <th scope="row" class="text-center text-black-50"><?Php echo $book['book_id'] ;?>
-              </th>
-              <td>
-                <?Php echo $book['title'] ;?>
-               </td>
-              <td>
-                <?Php echo $book['author_name'] ;?>
-              </td>
-              <td class="text-center">
-                <?Php echo $book['released_year'] ;?>
-              </td>
-            </tr>
-                <?php endforeach ;?>
-              </tbody>
-            
-          </table>
-       </div>
-      <?php include 'inc/footer.php'; ?>
+<div class="row col-12 mx-auto mt-4 mb-0">
+  <?php if(empty($books)): ?>
+    <p class="lead text-primary p-2 text-center fw-bold">
+      THERE IS NO Book AVAILABLE
+    </p>
+  <?php endif; ?>
+  <table class="table table-striped table-bordered table-hover table-responsive m-0 p-0">
+    <thead class="">
+      <tr class="bg-secondary text-white shadow">
+        <th scope="col" width="5%">S/N</th>
+        <th scope="col" width="55%" class="text-center">Title</th>
+        <th scope="col" width="30%" class="text-center">Author</th>
+        <th scope="col" width="10%">Released Year</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach($books as $book): ?>
+        <tr class="book_row" name="book_row" onclick="window.location.href='each_book_info.php?book_id=<?=$book['book_id'] ?>'">
+          <th scope="row" class="text-center text-black-50"><?php echo $book['book_id']; ?></th>
+          <td><?php echo $book['title']; ?></td>
+          <td><?php echo $book['author_name']; ?></td>
+          <td class="text-center"><?php echo $book['released_year']; ?></td>
+        </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+</div>
+<?php include 'inc/footer.php'; ?>
